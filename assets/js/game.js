798d82428464f6fbc4241041b8958b993454767f
Tphorton14 <Tphorton14@gmail.com>
2019-07-23 08:24:21 -0400

    recent update

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,63 @@
+
+//Declare global objects 
+let attack;
+let defense;
+let attackCharater;
+let attackerHealth;
+let defenseCharactor;
+let defenderHealth;
+let name;
+let myPlayer = "";
+let myDef = "";
+
+const resetBtn = $(".resetButton")
+
+
+
+const character = {
+    ObiWan: {
+        name: "Obiwan",
+        hP: 120,
+        attackPower: 8,
+        counterAttack: 24
+        
+    };
+
+    Rey: {
+        name: "Rey",
+        hP: 100,
+        attackPower: 10,
+        counterAttack: 5
+    };
+
+    darthVadar: {
+        name: "DarthVadar",
+        hP: 150,
+        attackPower: 10,
+        counterAttack: 25
+    };
+
+    kyloRyen: {
+        name: "KyloRyen",
+        hP: 180,
+        attackPower: 12,
+        counterAttack: 25
+    }
+}
+
+function reset () {
+
+    $("#playerPick").show();
+
+
+}
+
+
+
+
+//
+
+
 $(document).on("click", function () {
     console.log("clicked");
 });
