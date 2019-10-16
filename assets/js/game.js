1237e038875b81bee6df72a93b43c91917107137
Tphorton14 <Tphorton14@gmail.com>
2019-10-16 13:55:26 -0400

    app working, but still needs pictures

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,4 @@
 $(document).ready(function() { 
-
-
-
-//Declare global objects 
-
-// let attack;
-// let defense;
-// let power;
-// let attackCharater;
-// let attackerHealth;
-// let defenseCharactor;
-// let defenderHealth;
-// let name;
-// let yourCharacter;
-// let badCharacter;
-// let myPlayer = "";
-// let myDef = "";
-
-
-
-
-
 let characters = {
     "ObiWan": {
         name: "Obiwan",
@@ -39,7 +17,7 @@ let characters = {
         enemyAttackBack: 5
     },
 
-    "darthVadar": {
+    "Darth Vadar": {
         name: "Darth Vadar",
         health: 150,
         attack: 10,
@@ -47,7 +25,7 @@ let characters = {
         enemyAttackBack: 25
     },
 
-    "kyloRyen": {
+    "Kylo Ryen": {
         name: "Kylo Ryen",
         health: 180,
         attack: 12,
@@ -62,136 +40,129 @@ let defender;
 let turnCounter = 1;
 let killCount = 0;
 
-let renderCharacter = function(characters, renderArea) {
-    let charDiv = $("<div class='character' data-name='" + characters.name + "'>");
-    let charName = $("<div class='character-name'>").text(characters.name);
-                                                                                // need to take out hardcode and put image into JS file
-    let charImage = $("<img class='character-image'>").attr("src", characters.imageURL);
-    let charHealth = $("<div class='character-health'>").text(characters.health);
+let renderCharacter = function(character, renderArea) {
+    let charDiv = $("<div class='character' data-name='" + character.name + "'>");
+    let charName = $("<div class='character-name'>").text(character.name);
+    let charImage = $("<img alt-'image' class='character-image'>").attr("src", character.imageURL);
+    let charHealth = $("<div class='character-health'>").text(character.health);
     charDiv.append(charName).append(charImage).append(charHealth);
     $(renderArea).append(charDiv);
 };
 
-var initializeGame = function () {
-    for (let key in characters) {
-        renderCharacter(characters[key], "#characters-section");
+let initializeGame = function() {
+    for (var key in characters) {
+      renderCharacter(characters[key], "#characters-section");
     }
-};
+  };
 
-initializeGame();
+  initializeGame();
 
-var updateCharacter = function (charObj, areaRender) {
+  let updateCharacter = function(charObj, areaRender) {
     $(areaRender).empty();
     renderCharacter(charObj, areaRender);
-};
+  };
 
-var renderEnemies = function (enemyArr) {
+let renderEnemies = function(enemyArr) {
     for (let i = 0; i < enemyArr.length; i++) {
-        renderCharacter(enemyArr[i], "#available-to-attack-section");
+      renderCharacter(enemyArr[i], "#available-to-attack-section");
     }
-};
+  };
 
-var renderMessage = function(message) {
+  let renderMessage = function(message) {
     let gameMessageSet = $("#game-message");
-    let newMessage = $("div").text(message);
+    let newMessage = $("<div>").text(message);
     gameMessageSet.append(newMessage);
-}
+  };
 
-var restartGame = function (resultMessage) {
+let restartGame = function(resultMessage) {
+ 
     let restart = $("<button>Restart</button>").click(function() {
-        location.reload();
+      location.reload();
     });
 
     let gameState = $("<div>").text(resultMessage);
 
     $("body").append(gameState);
     $("body").append(restart);
-};
+  };
 
-var clearMessage = function () {
+  let clearMessage = function() {
     let gameMessage = $("#game-message");
 
     gameMessage.text("");
-};
+  };
 
-$("#characters-section").on("click", ".character", function () {
+
+
+
+
+
+
+
+
+
+
+$("#characters-section").on("click", ".character", function() {
     let name = $(this).attr("data-name");
-
-    if(!attacker){
-        attacker = characters[name];
-
-        for (let key in characters){
-            if (key !== name) {
-                combatants.push(characters[key]);
-            }
+    if (!attacker) {
+      attacker = characters[name];
+      for (let key in characters) {
+        if (key !== name) {
+          combatants.push(characters[key]);
         }
-
-        $("#characters-section").hide();
-
-        updateCharacter(attacker, "#selected-character");
-        renderEnemies(combatants);
+      }
+      $("#characters-section").hide();
+      updateCharacter(attacker, "#selected-character");
+      renderEnemies(combatants);
     }
-});
+  });
 
-
-$("#available-to-attack-section").on("click", ".character", function(){
-
+  $("#available-to-attack-section").on("click", ".character", function() {
     let name = $(this).attr("data-name");
+    if ($("#defender").children().length === 0) {
+      defender = characters[name];
+      updateCharacter(defender, "#defender");
+      $(this).remove();
+      clearMessage();
+    }
+  });
 
-    if($("#defender").children().length === 0){
-        defender = characters[name];
+
+  $("#attack-button").on("click", function() {
+    if ($("#defender").children().length !== 0) {
+      var attackMessage = "You attacked " + defender.name + " for " + attacker.attack * turnCounter + " damage.";
+      var counterAttackMessage = defender.name + " attacked you back for " + defender.enemyAttackBack + " damage.";
+      clearMessage();
+      defender.health -= attacker.attack * turnCounter;
+      if (defender.health > 0) {
         updateCharacter(defender, "#defender");
-
-        $(this).remove();
-        clearMessage();
-    }
-});
-
-$("#attack-button").on("click", function() {
-    if($("#defender").children().length !== 0) {
-        let attackMessage = "You Attacked " + defender.name  + " for " + attacker.attack * turnCounter + " damage.";
-        let counterAttackMessage = defender.name + " attacked you back for " + defender.enemyAttackBack + " damage.";
-        clearMessage();
-
-        defender.health -+ attacker.attack * turnCounter;
-
-        if (defender.health > 0) {
-            updateCharacter(defender, "#defender");
-
-            renderMessage(attackMessage);
-            renderMessage(counterAttackMessage);
-
-            attacker.health -= defender.enemyAttackBack;
-
-            updateCharacter(attacker, "#selected-character");
-
-            if (attacker.health <= 0) {
-                clearMessage();
-                restartGame("LOSER, you have lost!");
-                $("#attack-button").off("click");
-            }
-        } else {
-            $("#defender").empty();
-
-            let gameStateMessage = "You have defeated " + defender.name + ", please choose another enemy.";
-            renderMessage(gameStateMessage);
-
-            killCount++;
-
-            if (killCount >= combatants.length) {
-                clearMessage();
-                $("#attack-button").off("click");
-                restartGame("Congrats, you have WON!");
-            }
+        renderMessage(attackMessage);
+        renderMessage(counterAttackMessage);
+        attacker.health -= defender.enemyAttackBack;
+        updateCharacter(attacker, "#selected-character");
+        if (attacker.health <= 0) {
+          clearMessage();
+          restartGame("You have been defeated...GAME OVER!!!");
+          $("#attack-button").off("click");
         }
+      }
+      else {
+        $("#defender").empty();
 
-        turnCounter++;
-    } else {
-        clearMessage();
-        renderMessage ("No more enemies to fight here.");
+        var gameStateMessage = "You have defeated " + defender.name + ", you can choose to fight another enemy.";
+        renderMessage(gameStateMessage);
+        killCount++;
+        if (killCount >= combatants.length) {
+          clearMessage();
+          $("#attack-button").off("click");
+          restartGame("You Won!!!! GAME OVER!!!");
+        }
+      }
+      turnCounter++;
     }
-});
-
-
-
+    else {
+      clearMessage();
+      renderMessage("Please choose a enemy!");
+    }
+  });
 });
